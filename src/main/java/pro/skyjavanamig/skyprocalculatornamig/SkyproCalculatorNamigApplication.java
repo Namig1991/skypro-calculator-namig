6ec2349560917aef2676caf257fdf63bf4930e76
Namig1991 <safarovnamig09@gmail.com>
2022-03-28 13:34:31 +0300

    Мой первый коммит

---
+++ src/main/java/pro/skyjavanamig/skyprocalculatornamig/SkyproCalculatorNamigApplication.java
@@ -0,0 +1,13 @@
+package pro.skyjavanamig.skyprocalculatornamig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyproCalculatorNamigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyproCalculatorNamigApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/skyjavanamig/skyprocalculatornamig/SkyproCalculatorNamigApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.skyjavanamig.skyprocalculatornamig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyproCalculatorNamigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
